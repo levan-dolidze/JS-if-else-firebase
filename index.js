9e0ceeb355a01c44856b5bb4d91aa3100cc70c79
DESKTOP-H8VL884\levan <levan.dolidze@outlook.com>
2020-08-25 23:17:36 +0400

    JS if else and firebase

---
+++ index.js
@@ -0,0 +1,106 @@
+// var nam="levani";
+// var sur ="dolidze";
+// var sum = nam+" "+sur;
+// console.log(sum);
+
+
+
+// var name = prompt("შენი სახელი?");
+// var surname =prompt("შენი გვარი?");
+// var age =prompt("ასაკი?")
+
+
+
+// // document.getElementById("test").innerHTML="გამარჯობა "+ name +" "+ surname;
+
+// document.getElementsByClassName("test2").innerHTML="hi" + name+" "+surname;
+
+// var hour =21;
+
+// if(hour>=6 && hour<12){
+//     console.log("good morning");
+// }
+// else if  (hour>=12 && hour<18)
+// {
+//     console.log("good afternoon");
+// }
+// else{
+//     console.log("good evening");
+// }
+
+
+
+
+
+// // Your web app's Firebase configuration
+// var firebaseConfig = {
+//   apiKey: "AIzaSyCaMBhvmLNnPDUoiiLMV5X7HTFypP8XKFE",
+//   authDomain: "data1-1c5d6.firebaseapp.com",
+//   databaseURL: "https://data1-1c5d6.firebaseio.com",
+//   projectId: "data1-1c5d6",
+//   storageBucket: "data1-1c5d6.appspot.com",
+//   messagingSenderId: "84965172617",
+//   appId: "1:84965172617:web:a7bc178d45cecf74782d58",
+//   measurementId: "G-P3Q4YF3HQ8"
+// };
+
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+// var messagesRef = firebase.database().ref("messages");
+
+
+
+// document.getElementById("mainform").addEventListener("submit" , submitForm);
+
+// function submitForm(e){
+//     e.preventDefault();
+   
+//     var name=getInputVal("name");
+//     var lastname=getInputVal("lastname");
+//     saveMessages(name,lastname);
+
+// }
+
+// function getInputVal(id){
+//     return document.getElementById(id).Value;
+// }
+
+// function saveMessages (name,lastname){
+//     var newMessageRef=messageRef.push();
+//     newMessageRef.set({
+
+//         name:name,
+//         lastname:lastname
+//     });
+// }
+
+
+// var name ="levan";
+// var age = 30;
+
+// age/=10;
+// console.log(name,age);
+
+// var isMale=true;
+
+
+// var hoby = ["lamazo","cars"]
+
+// console.log(hoby);
+
+// var object ={ 
+
+//   name:'name',
+//   surname:"mari"
+// }
+// console.log(object);
+
+// var pet
+// pet="amaoz"
+
+// var name ="levani";
+// document.getElementById("welcome").innerHTML="hello "+ name+"welcome"
+
+// confirm(name+"ravaxa s?")
+
